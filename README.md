3f871f45331e307e659289d477b624e5f341894f
Melissa <mee_seren@outlook.com>
2019-04-29 00:07:38 -0300

    Finalização

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # caixaeletronico
 Desenvolver um sistema que funciona como um caixa eletrônico.
+
+Dados para acesso: 
+
+E-mail: admin@hotmail.com
+Senha: 12345678
---
+++ caixaeletronico.sql/caixaeletronico_depositos.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: caixaeletronico
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `depositos`
+--
+
+DROP TABLE IF EXISTS `depositos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `depositos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `agencia` int(11) NOT NULL,
+  `conta` int(11) NOT NULL,
+  `valor` double(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `depositos`
+--
+
+LOCK TABLES `depositos` WRITE;
+/*!40000 ALTER TABLE `depositos` DISABLE KEYS */;
+INSERT INTO `depositos` VALUES (1,123,12,1.00),(2,234,213,1500.00),(3,234,213,1500.00),(4,4324,324,234.00),(5,4324,324,234.00),(6,423,234,123.00),(7,423,234,123.00),(8,423,234,123.00);
+/*!40000 ALTER TABLE `depositos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-29  0:05:43
